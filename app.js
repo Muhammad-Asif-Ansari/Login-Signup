6d0f2b16f913f9262b9e3e4672d00f7e3b1380a4
Muhammad-Asif-Ansari <muhammadasifansari101@gmail.com>
2024-12-02 21:53:16 +0500

    login & Signup form store in local storage

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 var inputName = document.getElementById("inputName")
 var inputPassword = document.getElementById("inputPassword")
 function signup(){
@@ -8,4 +7,37 @@ localStorage.setItem("inputName",inputName.value)
 localStorage.setItem("inputPassword",inputPassword.value)
 inputName.value = ""
 inputPassword.value = ""
+Swal.fire({
+    position: "top-end",
+    icon: "success",
+    title: "Sign Up Successfully",
+    showConfirmButton: false,
+    timer: 1500
+  })
 }
+
+
+function login(){
+var userName = document.getElementById("userName").value
+var password = document.getElementById("password").value
+var inputValue = localStorage.getItem("inputName")
+var inputValuePassword = localStorage.getItem("inputPassword")
+
+
+if(userName === inputValue && password === inputValuePassword ){
+    Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Login Successfully",
+        showConfirmButton: false,
+        timer: 1500
+      })      
+}
+else{
+    Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Login Failed!",
+        footer: '<a href="#">Why do I have this issue?</a>'
+      });
+}}
